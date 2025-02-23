b450b0cd242f005f6f14b6f1d01645aed2c97aad
Hu Tao 128 <alcantarahuerta128@gmail.com>
2025-02-22 23:09:19 -0800

    ya quedo

--- App.js
+++ App.js
@@ -13,6 +13,7 @@ import QuoteScreen from './src/screens/QuoteScreen';
 import ProfileScreen from './src/screens/ProfileScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import OnboardingScreen from './src/screens/OnboardingScreen';
+import RegisterScreen from './src/screens/RegisterScreen';
 
 import { UserProvider, useUser } from './src/context/userContext';
 
@@ -62,6 +63,7 @@ const MainNavigator = () => {
       {!user ? (
         <>
           <Stack.Screen name="Onboarding" component={OnboardingScreen} />
+          <Stack.Screen name="Register" component={RegisterScreen} />
           <Stack.Screen name="Login">
             {props => <LoginScreen {...props} onLogin={setUser} />}
           </Stack.Screen>
